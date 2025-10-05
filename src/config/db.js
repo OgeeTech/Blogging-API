50f379f96ca5d962ca00a58addf7a5826b805528
Wada Gift <wgift41@gmail.com>
2025-10-05 14:58:46 +0100

    done

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 
 module.exports = async function connectDB() {
     const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/blogging-api';
-    await mongoose.connect(uri);
-    console.log('MongoDB connected');
+    try {
+        await mongoose.connect(uri, {
+          
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('Failed to connect DB:', err);
+        process.exit(1); // stop the server if DB connection fails
+    }
 };
